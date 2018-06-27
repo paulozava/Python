489c5c77aaa65edd8d511d5d17b4c3256db3a495
paulozava <camposzava@gmail.com>
2018-06-27 13:28:16 +0200

    corrections in method embarking
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CW_the_lift.py
+++ CW_the_lift.py
@@ -1,18 +1,17 @@
 # https://www.codewars.com/kata/the-lift/train/python
 
 
-
 class Dinglemouse(object):
 
     def __init__(self, queues, capacity):
-        self.queues = queues
+        self.queues = list(map(list, queues))
         self.capacity = capacity
         self.capacity_used = 0
         self.actual_floor = 0
         self.build_floors = len(queues)
         self.motion = self.get_motion()
         self.buttons_pressed = self.get_calls(queues)
-        self.stop_needs = {}
+        self.stop_needs = {floor: 0 for floor, queue in enumerate(queues)}
 
     def get_motion (self):
         if self.actual_floor < self.build_floors:
@@ -33,10 +32,17 @@ class Dinglemouse(object):
         return buttons_pressed
     
     def embarking (self, floor_orders):
-        passagers = []
+        filter_possibilities = {'up': lambda x: x > self.actual_floor,
+                                'down': lambda x: x < self.actual_floor}
+        possible_orders = list(filter(filter_possibilities[self.motion], floor_orders))
         while self.capacity > self.capacity_used:
-            self.capacity_used += 1
-            passager_order = floor_orders.pop()
+            if possible_orders:
+                self.capacity_used += 1
+                passager_order = possible_orders.pop()
+                self.stop_needs[passager_order] += 1
+                floor_orders.remove(passager_order)
+            else: break
+        return floor_orders
 
 
     # g-0   1   2         3   4   5   6
