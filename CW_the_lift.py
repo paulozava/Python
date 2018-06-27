686f589a9920095a5e0eb2209a350a941dca2d67
paulozava <camposzava@gmail.com>
2018-06-27 10:48:16 +0200

    add function get_calls
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CW_the_lift.py
+++ CW_the_lift.py
@@ -5,4 +5,31 @@ class Dinglemouse(object):
     def __init__(self, queues, capacity):
         self.queues = queues
         self.capacity = capacity
+        self.build_floors = len(queues)
+        self.actual_floor = 0
+        self.motion = 'up'
+        self.buttons_pressed = get_calls(queues)
 
+
+    def get_calls (queues):
+        buttons_pressed = {}
+        for floor, queue in enumerate(queues):
+            floor_button = {'up': False, 'down': False}
+            for call in queue:
+                if call > floor:
+                    floor_button['up'] = True
+                else:
+                    floor_button['down'] = True
+            buttons_pressed.update({floor : floor_button})
+        return buttons_pressed
+
+    # g-0   1   2         3   4   5   6
+    # ((), (), (5, 5, 5), (), (), (), ())
+    def theLift(self):
+        checagem de pedidos
+        movimento
+        carregamento
+        descarregamento
+        reversao do movimento
+        sem novos pedidos
+        return []
