15fef97f73a9aaba334ecaae0dc276d2448be9a5
paulozava <camposzava@gmail.com>
2018-06-27 11:12:43 +0200

    add method embarking
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CW_the_lift.py
+++ CW_the_lift.py
@@ -7,10 +7,12 @@ class Dinglemouse(object):
     def __init__(self, queues, capacity):
         self.queues = queues
         self.capacity = capacity
-        self.build_floors = len(queues)
+        self.capacity_used = 0
         self.actual_floor = 0
-        self.motion = 'up'
+        self.build_floors = len(queues)
+        self.motion = self.get_motion()
         self.buttons_pressed = self.get_calls(queues)
+        self.stop_needs = {}
 
     def get_motion (self):
         if self.actual_floor < self.build_floors:
@@ -18,9 +20,9 @@ class Dinglemouse(object):
         else:
             return 'down'
 
-    def get_calls (self, queues):
+    def get_calls (self):
         buttons_pressed = {}
-        for floor, queue in enumerate(queues):
+        for floor, queue in enumerate(self.queues):
             floor_button = {'up': False, 'down': False}
             for call in queue:
                 if call > floor:
@@ -29,11 +31,17 @@ class Dinglemouse(object):
                     floor_button['down'] = True
             buttons_pressed.update({floor : floor_button})
         return buttons_pressed
+    
+    def embarking (self, floor_orders):
+        passagers = []
+        while self.capacity > self.capacity_used:
+            self.capacity_used += 1
+            passager_order = floor_orders.pop()
+
 
     # g-0   1   2         3   4   5   6
     # ((), (), (5, 5, 5), (), (), (), ())
     def theLift(self):
-        movimento
         carregamento
         descarregamento
         reversao do movimento
