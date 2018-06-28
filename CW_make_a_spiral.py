1507510c3fe8ae5049a7971aa512b06686cd7ca1
paulozava <camposzava@gmail.com>
2018-06-28 13:17:23 +0200

    define a rotate function that can rotate a n-dimensional matrix in clockwise sense or anti-clockwise sense
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CW_make_a_spiral.py
+++ CW_make_a_spiral.py
@@ -1,9 +1,33 @@
+# https://www.codewars.com/kata/make-a-spiral/train/python
+
 def spiralize(size):
+    snake_max_path = size
     spital_matrix = make_spiral_matrix(size)
+    for path in range(snake_max_path):
+
+    spital_matrix = rotate(spital_matrix)
     return spital_matrix
 
 
 def make_spiral_matrix(size):
-    vertical = [0] * size
-    horizontal = [vertical for i in range(size)]
-    return horizontal
+    matrix = [[0] * size for i in range(size)]
+    return matrix
+
+def rotate (matrix, clockwise=True):
+    new_matrix = []
+    size = len(matrix)
+    for horizontal in range(size):
+        new_line = []
+        for vertical in range(size):
+            element = matrix[vertical][horizontal]
+            new_line.append(element)
+        if clockwise:
+            new_line.reverse()
+        new_matrix.append(new_line)
+    if not clockwise:
+        new_matrix.reverse()
+    return new_matrix
+
+def test (matrix, size):
+    matrix[0] = [1 for i in range(size)]
+    matrix[-1] = [1 for i in range(size)]
