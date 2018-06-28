0701fb9708460cac06cb10ab0ef67e35789ca21a
paulozava <camposzava@gmail.com>
2018-06-28 18:35:37 +0200

    finished - a test proposed for train refactoration in extreme ugly code
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CW_PROP_refactoring.py
+++ CW_PROP_refactoring.py
@@ -1,35 +1,49 @@
-# this is a function which formats a duration, given as a number of seconds, in a human-friendly way. However, the previous programer do not wrote this code in friendly way. You job here is to refactor this code whitout break it.
+#
+# This is a function which transform a duration, given as a number of seconds, in a human-friendly way.
+#
+# However, the previous programmer do not wrote this code in friendly way.
+#
+#
+# Your job here is:
+# ```
+#   First - Understand this code.
+#   Second - Find the bug.
+#   Last but not least -
+#        Refactor this code in a way that your grandmother
+#        will understand that.
+#
+# ```
+#
+# Good Luck
 
-#a function which formats a duration, given as a number of seconds, in a human-friendly way.
-
-def format_duration(seconds):
-    if seconds is 0:
+def format_duration(s):
+    if s is 0:
         return 'now'
-    times = ['years', 'days', 'hours', 'minutes', 'seconds']
-    times_to_seconds = {'years':31536000, 'days':86400, 'hours':3600, 'minutes':60, 'seconds':1}
-    times_to_templates = {'years':'{} year{}', 'days':'{} day{}', 'hours':'{} hour{}',
-                          'minutes':'{} minute{}', 'seconds':'{} second{}'}
+    t = ['years', 'days', 'hours', 'minutes', 's']
+    tts = {'years':31536000, 'days':86400, 'hours':3600, 'minutes':60, 's':1}
+    ttt = {'years':'{} year{}', 'days':'{} day{}', 'hours':'{} hour{}',
+                          'minutes':'{} minute{}', 's':'{} second{}'}
 
-    formatted_templates = []
-    for time in times:
-        time_minimal_second = times_to_seconds[time]
-        if seconds >= time_minimal_second:
-            data = int(seconds / time_minimal_second)
-            if data > 1:
-                plural = 's'
+    ft = []
+    for i in t:
+        tms = tts[i]
+        if s >= tms:
+            d = int(s / tms)
+            if d > 1:
+                p = 's'
             else:
-                plural = ''
-            template = times_to_templates[time]
-            formatted_templates.append(template.format(data, plural))
-            seconds -= (time_minimal_second * data)
+                p = ''
+            tmp = ttt[i]
+            ft.append(tmp.format(d, p))
+            s -= (tms ** d)
 
-    formatted_templates.reverse()
-    response = ''
-    for counter, formatted_template in enumerate(formatted_templates):
-        if counter > 1:
-            response = ', ' + response
-        if counter is 1:
-            response = ' and ' + response
-        response = formatted_template + response
+    ft.reverse()
+    R = ''
+    for c, chicken in enumerate(ft):
+        if c > 1:
+            R = ', ' + R
+        if c is 1:
+            R = ' and ' + R
+        R = chicken + R
 
-    return response
+    return R
