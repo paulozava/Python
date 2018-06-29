a91d8ae525150b541fab132680408190d8ce4659
paulozava <camposzava@gmail.com>
2018-06-29 09:21:37 +0200

    add function bool_to_word
    Signed-off-by: paulozava <camposzava@gmail.com>

---
+++ CW_challenges/CW_8_basic_solutions.py
@@ -0,0 +1,12 @@
+
+#get an array of numbers, return the sum of all of the positives ones.
+def positive_sum(numbers):
+    positive_numbers = filter(lambda x: x>0, numbers)
+    list_of_positive_numbers = list(positive_numbers)
+    sum_of_it = sum(list_of_positive_numbers)
+    return sum_of_it
+
+#takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+def bool_to_word(boolean):
+    if boolean: return 'Yes'
+    return 'No'
