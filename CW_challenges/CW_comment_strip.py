6c674006ec4f7ba1d374b4869bfc4a2041806874
paulozava <camposzava@gmail.com>
2018-06-29 08:55:23 +0200

    Finished - a function that receive a string an some markers and return this string without this markers
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CW_challenges/CW_comment_strip.py
+++ CW_challenges/CW_comment_strip.py
@@ -1,3 +1,5 @@
+# a function that receive a string an some markers and return this string without this markers
+
 def solution(string,markers):
     parts = string.split('\n')
     for mark in markers:
