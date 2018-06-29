10f571480139ebdaeeda0a0ef5ed5702400c9127
paulozava <camposzava@gmail.com>
2018-06-29 08:55:47 +0200

    Finished - algorithm that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CW_challenges/CW_format_range_numbers.py
+++ CW_challenges/CW_format_range_numbers.py
@@ -1,7 +1,5 @@
 # algorithm that it takes a list of integers in increasing order and returns a correctly formatted string in the range format.
-# solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
 
-# returns "-6,-3-1,3-5,7-11,14,15,17-20"
 def solution(args):
     begin, rest = args[0], args[1:]
     ender = []
