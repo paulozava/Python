2ca05001d0a39f40de3ae583689e8430ef99288a
paulozava <camposzava@gmail.com>
2018-07-02 13:29:01 +0200

    Complete the function inver_parentesis
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CF_challenges/reversed_parentesis.py
+++ CF_challenges/reversed_parentesis.py
@@ -1,5 +1,9 @@
 import re
 
 def invert_parentesis(string):
-    re.findall(r'\((^\(\))\)', string)
-    re.findall(r'\((\w+\s*)\)', string)
+    find_patterns = re.findall(r'\(([\w\s!?@#$%&*]*)\)', string)
+    for pattern in find_patterns:
+        reversed_string = pattern[::-1]
+        complete_pattern = '(' + pattern + ')'
+        string = string.replace(complete_pattern, reversed_string)
+    return string
