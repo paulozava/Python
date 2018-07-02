e4fdbec3da46b70864a1213ef1c4debfa2cdf6f4
paulozava <camposzava@gmail.com>
2018-07-02 13:30:32 +0200

    improvement in function inver_parentesis
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CF_challenges/reversed_parentesis.py
+++ CF_challenges/reversed_parentesis.py
@@ -1,9 +1,10 @@
 import re
 
 def invert_parentesis(string):
-    find_patterns = re.findall(r'\(([\w\s!?@#$%&*]*)\)', string)
-    for pattern in find_patterns:
-        reversed_string = pattern[::-1]
-        complete_pattern = '(' + pattern + ')'
-        string = string.replace(complete_pattern, reversed_string)
+    while "(" in string
+        find_patterns = re.findall(r'\(([\w\s!?@#$%&*]*)\)', string)
+        for pattern in find_patterns:
+            reversed_string = pattern[::-1]
+            complete_pattern = '(' + pattern + ')'
+            string = string.replace(complete_pattern, reversed_string)
     return string
