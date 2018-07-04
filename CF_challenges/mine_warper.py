a2d90be7cad99e17f2f9712d39963443ea3ac1d1
paulozava <camposzava@gmail.com>
2018-07-04 09:53:54 +0200

    Finished function that count mines in neighbourhood
    Signed-off-by: paulozava <camposzava@gmail.com>

---
+++ CF_challenges/mine_warper.py
@@ -0,0 +1,19 @@
+def minesweeper(matrix):
+    x_axis, y_axis = len(matrix[0]), len(matrix)
+    new_matrix = []
+    for y in range(y_axis):
+        line = []
+        for x in range(x_axis):
+            neighbour = []
+            if x > 0:
+               neighbour.extend(matrix[y][x-1])
+            if x < x_axis -1:
+                neighbour.extend(matrix[y][x + 1])
+            if y > 0:
+                neighbour.extend(matrix[y-1][x])
+            if y < y_axis -1:
+                neighbour.extend(matrix[y+][x])
+            mines = neighbour.count(True)
+            line.append(mines)
+        new_matrix.append(line)
+    return new_matrix
