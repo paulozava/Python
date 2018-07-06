8960a4f370299e062a0c6954c5b58b48c552356a
paulozava <camposzava@gmail.com>
2018-07-06 11:03:35 +0200

    add fish in a pound simulation
    Signed-off-by: paulozava <camposzava@gmail.com>

---
+++ CW_challenges/CW_Fish_in_the_pond.py
@@ -0,0 +1,28 @@
+# https://www.codewars.com/kata/5904be220881cb68be00007d/train/python
+
+#  	9.  Your size increments by one each time you reach the amounts below.
+
+def fish(shoal, size = 1, ate_protein = 0):
+    #  	1.  Your size starts at 1
+    #  	2.  The shoal string will contain fish integers between 0-9
+    #  	3.  0 = algae and wont help you feed.
+    #  	4.  The fish integer represents the size of the fish (1-9).
+    fishes = [int(fish) for fish in shoal if fish != '0']
+    #  	5.  You can only eat fish the same size or less than yourself.
+    edible_fishes = filter(lambda x: x <= size, fishes)
+    #  	6.  You can eat the fish in any order you choose to maximize your size.
+    #  	7.  You can and only eat each fish once.
+    protein_earned = sum(edible_fishes)
+    ate_protein += protein_earned
+    #can grow?
+    if ate_protein >= protein_to_grow(size):
+        size += 1
+
+    #  	8.  The bigger fish you eat, the faster you grow. A size 2 fish equals two size 1 fish, size 3 fish equals three size 1 fish, and so on.
+
+
+def protein_to_grow(actual_size):
+    sizes = [1,2,3,4,5,6,7,8,9]
+    accumulate_sizes = filter(lambda x: x <= actual_size, sizes)
+    protein_to_accumulate = sum([4*size for size in accumulate_sizes])
+    return protein_to_accumulate
