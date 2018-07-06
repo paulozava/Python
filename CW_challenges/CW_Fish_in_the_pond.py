b1e4db59e75ffad33d9a6ec322a13dabe609bf20
paulozava <camposzava@gmail.com>
2018-07-06 13:16:10 +0200

    add in_script tests
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CW_challenges/CW_Fish_in_the_pond.py
+++ CW_challenges/CW_Fish_in_the_pond.py
@@ -2,7 +2,7 @@ def fish(shoal, size = 1, protein_earned = 0):
     fishes = [int(fish) for fish in shoal if fish != '0']
     grow = True
     while grow:
-        edible_fishes = filter(lambda x: x <= size, fishes)
+        edible_fishes = list(filter(lambda x: x <= size, fishes))
         ate_protein = sum(edible_fishes)
         protein_earned += ate_protein
         if protein_earned >= protein_to_grow(size):
@@ -20,21 +20,21 @@ def protein_to_grow(actual_size):
 
 
 if __name__ == '__main__':
-    # print(fish(""))
-    # print("Should return '1'")
-    # print(fish("0"))
-    # print("Should return '1'")
-    # print(fish("6"))
-    # print("Should return '1'")
-    # print(fish("1111"))
-    # print("Should return '2'")
+    print(fish(""))
+    print("Should return '1'")
+    print(fish("0"))
+    print("Should return '1'")
+    print(fish("6"))
+    print("Should return '1'")
+    print(fish("1111"))
+    print("Should return '2'")
     print(fish("11112222"))
     print("Should return '3'")
-    # print(fish("111122223333"))
-    # print("Should return '4'")
-    # print(fish("111111111111"))
-    # print("Should return '3'")
-    # print(fish("111111111111111111112222222222"))
-    # print("Should return '5'")
-    # print(fish("151128241212192113722321331"))
-    # print("Should return '5'")
+    print(fish("111122223333"))
+    print("Should return '4'")
+    print(fish("111111111111"))
+    print("Should return '3'")
+    print(fish("111111111111111111112222222222"))
+    print("Should return '5'")
+    print(fish("151128241212192113722321331"))
+    print("Should return '5'")
