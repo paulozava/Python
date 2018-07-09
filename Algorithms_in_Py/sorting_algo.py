f3f821c2987bd36c23c2d8436ff969b0a265c729
paulozava <camposzava@gmail.com>
2018-07-09 15:50:27 +0200

    Implemented selection sort
    Signed-off-by: paulozava <camposzava@gmail.com>

--- Algorithms_in_Py/sorting_algo.py
+++ Algorithms_in_Py/sorting_algo.py
@@ -2,9 +2,10 @@ def reverse_it (iterator):
     new_iterator = []
     while iterator:
         new_iterator.append(iterator.pop())
+    iterator.append(new_iterator)
     return iterator
 
-def bubble(iterator, reverse=False):
+def bubble(iterator=None):
     iterator_length = len(iterator)
     for i in range(0, iterator_length):
         swapped = False
@@ -16,7 +17,7 @@ def bubble(iterator, reverse=False):
             break
     return iterator
 
-def insertion_sort(iterator):
+def insertion_sort(iterator=None):
     # Stable
     # O(1) extra space
     # O(n2) comparisons and swaps
@@ -29,3 +30,17 @@ def insertion_sort(iterator):
             iterator[j], iterator[j - 1] = iterator[j - 1], iterator[j]
             j -= 1
     return reverse_it(iterator)
+
+def selection (iterator=None):
+    # Not stable
+    # O(1) extra space
+    # Θ(n2) comparisons
+    # Θ(n) swaps
+    # Not adaptive
+    iterator_length = len(iterator)
+    for i in range(iterator_length):
+        k = i
+        for j in range(i+1, iterator_length):
+            if iterator[j] < iterator[k]:
+                iterator[j], iterator[k] = iterator[k], iterator[j]
+    return iterator
