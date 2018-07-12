4346bbbce37e75a59044fb3b635ef68a722d294d
paulozava <camposzava@gmail.com>
2018-07-12 17:54:54 +0200

    data cleaning

--- Kaggle_challenges/wc2018.py
+++ Kaggle_challenges/wc2018.py
@@ -5,3 +5,7 @@ from matplotlib import pyplot as plt
 matches = pd.read_csv('results.csv')
 matches =  matches.replace({'Germany DR': 'Germany', 'China': 'China PR'})
 matches['date'] = pd.to_datetime(matches['date'])
+
+matches['date','home_team','away_team','home_score','away_score']
+matches['date' : 'away_score']
+
