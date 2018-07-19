b8d65ac7aaabe233a339ab8b65fd01bf45b66f49
paulozava <camposzava@gmail.com>
2018-07-19 16:16:44 +0200

    abrindo dados dataframe
    Signed-off-by: paulozava <camposzava@gmail.com>

--- AlCuOn/Pandas/aula01.py
+++ AlCuOn/Pandas/aula01.py
@@ -1,5 +1,18 @@
 import pandas
 
 alugueis = pandas.read_csv('/home/laumzav/PycharmProjects/Python_study/AlCuOn/Pandas/aluguel.csv', sep=';')
-alugueis.head()
-alugueis.tail(n=2)
+
+## observando a importação dos dados
+alugueis.head() #importa os primeiros
+alugueis.tail(n=2) #importa os últimos
+
+## mostrando os tipos de dados
+type(alugueis) # que objeto é
+alugueis.info() #informações sobre o dataframe do pandas
+alugueis.dtypes #mesma informação assima, mas só com os tipos de dados (object=>string; int64=>int; float64=> float) OBS. usar sem parenteses
+
+# embelezando o dataFrame
+tipos_dados = pandas.DataFrame(alugueis.dtypes, columns=['Tipos de dados']) #criando um DF para receber os dados
+tipos_dados.columns.name = 'Variável' #modificando a coluna de variáveis
+tipos_dados
+
