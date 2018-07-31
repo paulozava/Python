bcd0af263a411afabb7b40de5b54880832d12334
paulozava <camposzava@gmail.com>
2018-07-31 20:29:48 +0200

    Riemman's approximations to calculate an area above a curve
    Signed-off-by: paulozava <camposzava@gmail.com>

--- CW_challenges/CW_riemmans.py
+++ CW_challenges/CW_riemmans.py
@@ -1,51 +1,51 @@
-def left_riemman(function, n, a, b):
-    delta = (b - a) / n
+def left_riemman(function, retangles, start_point, end_point):
+    delta = (end_point - start_point) / retangles
     area = 0
-    for i in range(n):
-        area += function(a) * delta
-        a += delta
+    for i in range(retangles):
+        area += function(start_point) * delta
+        start_point += delta
     return area
 
-def right_riemman(function, n, a, b):
-    delta = (b - a) / n
+def right_riemman(function, retangles, start_point, end_point):
+    delta = (end_point - start_point) / retangles
     area = 0
-    for i in range(n):
-        a += delta
-        area += function(a) * delta
+    for i in range(retangles):
+        start_point += delta
+        area += function(start_point) * delta
     return area
 
-def midlle_riemman(function, n, a, b):
-    delta = (b - a) / n
-    a += delta/2
+def midlle_riemman(function, retangles, start_point, end_point):
+    delta = (end_point - start_point) / retangles
+    start_point += delta / 2
     area = 0
-    for i in range(n):
-        area += function(a) * delta
-        a += delta
+    for i in range(retangles):
+        area += function(start_point) * delta
+        start_point += delta
     return area
 
-def elements_left_riemman(function, n, a, b):
-    delta = (b - a) / n
+def elements_left_riemman(function, retangles, start_point, end_point):
+    delta = (end_point - start_point) / retangles
     area = []
-    for i in range(n):
-        left_point = function(a)
+    for i in range(retangles):
+        left_point = function(start_point)
         area.append(left_point)
-        a += delta
+        start_point += delta
     return area
 
-def elements_right_riemman(function, n, a, b):
-    delta = (b - a) / n
+def elements_right_riemman(function, retangles, start_point, end_point):
+    delta = (end_point - start_point) / retangles
     area = []
-    for i in range(n):
-        a += delta
-        right_point = function(a)
+    for i in range(retangles):
+        start_point += delta
+        right_point = function(start_point)
         area.append(right_point)
     return area
 
-def riemann_trapezoidal(function, n, a, b):
-    delta = (b - a) / n
-    left = elements_left_riemman(function, n, a, b)
-    right = elements_right_riemman(function, n, a, b)
+def riemann_trapezoidal(function, retangles, start_point, end_point):
+    delta = (end_point - start_point) / retangles
+    left_points = elements_left_riemman(function, retangles, start_point, end_point)
+    right_points = elements_right_riemman(function, retangles, start_point, end_point)
     area = 0
-    for r, l in zip(right, left):
-        area += ((r + l)/2)*delta
+    for right_point, left_point in zip(right_points, left_points):
+        area += ((right_point + left_point)/2)*delta
     return area
