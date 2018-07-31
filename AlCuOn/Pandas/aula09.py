9fdfbe8620145abf06172e34b75ae15afb334843
paulozava <camposzava@gmail.com>
2018-07-31 15:13:56 +0200

    fim do curso
    Signed-off-by: paulozava <camposzava@gmail.com>

--- AlCuOn/Pandas/aula09.py
+++ AlCuOn/Pandas/aula09.py
@@ -46,3 +46,17 @@ g2.set_title('Valores do aluguel')
 
 g3.hist(corte['Valor'].sample(100).reset_index())
 
+
+area = plt.figure()
+g1 = area.add_subplot(1, 2, 1)
+g2 = area.add_subplot(1, 2, 2)
+grupo1 = corte.groupby('Tipo')['Valor']
+label = grupo1.count().index
+valores = grupo1.count().values
+g1.pie(valores, labels = label, autopct='%1.1f%%')
+g1.set_title('Total de Imóveis por Tipo Agregado')
+grupo2 = corte.groupby('Bairro')['Valor']
+label = grupo2.count().index
+valores = grupo2.count().values
+g2.pie(valores, labels = label, autopct='%1.1f%%', explode = (.1, .1, .1, .1, .1))
+g2.set_title('Total de Imóveis por Tipo')
