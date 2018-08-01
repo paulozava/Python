bb347de4e7588dc10fddcab233387b9bc40f9019
paulozava <camposzava@gmail.com>
2018-08-01 14:09:47 +0200

    Find the position of a candy in a box

--- CW_challenges/CW_candy_in_boxes.py
+++ CW_challenges/CW_candy_in_boxes.py
@@ -1,2 +1,11 @@
+from math import ceil
+
 def get_candy_position(number_candies, row, col, find_candy):
-    
+    if find_candy > number_candies: return [-1,-1,-1]
+    box_spot = row * col
+    box = ceil(find_candy/box_spot)
+    row_c = row - ceil((find_candy % box_spot) / col)
+        if row_c == row: row_c = 0
+    col_c = col - (find_candy % col)
+    resp = [box, row_c, col_c]
+    return resp
