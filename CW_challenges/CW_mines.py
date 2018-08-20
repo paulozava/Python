2206460a8b8384efdfcb0ded16cd102e2c334d07
paulozava <camposzava@gmail.com>
2018-08-20 18:30:08 +0200

    problems with mines

--- CW_challenges/CW_mines.py
+++ CW_challenges/CW_mines.py
@@ -1,7 +1,17 @@
-def transpose(map):
-    xlen = len(map[0])
-    ylen = len(map)
-    for i in range(ylen):
+def rotate (matrix, clockwise=True):
+    new_matrix = []
+    size = len(matrix)
+    for horizontal in range(size):
+        new_line = []
+        for vertical in range(size):
+            element = matrix[vertical][horizontal]
+            new_line.append(element)
+        if clockwise:
+            new_line.reverse()
+        new_matrix.append(new_line)
+    if not clockwise:
+        new_matrix.reverse()
+    return new_matrix
 
 
 def solve_mine(map, n):
@@ -17,7 +27,9 @@ map = """
 """.strip()
 
 mapline = [line.split(' ') for line in map.split('\n')]
-
+map_transpose = rotate(mapline)
 
 for line in mapline:
-    if '0' in line:
+    for item in line:
+        if '0' == item:
+
