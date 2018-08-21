c67235592b9cc1e6e13af643061beae5eadd1b6e
paulozava <camposzava@gmail.com>
2018-08-21 16:08:04 +0200

    add 4 pass stations problem

---
+++ CW_challenges/CW_4pass.py
@@ -0,0 +1,5 @@
+def four_pass(stations):
+	#your code goes here. you can do it!
+	pass
+# if you prefer to see an overhead view of the factory floor with any failed test results, uncomment the line below:
+# show_graph_debug = True
